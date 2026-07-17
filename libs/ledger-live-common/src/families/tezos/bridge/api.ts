1447a62506bfd7bbcbc850c948932ddd2c825738
Aleksei Maslakov <alexei.maslakov@gmail.com>
2026-07-17 12:20:54 +0400

    fix(tezos): change import to type for TezosCoinConfig

--- libs/ledger-live-common/src/families/tezos/bridge/api.ts
+++ libs/ledger-live-common/src/families/tezos/bridge/api.ts
@@ -1,5 +1,5 @@
 import { createApi as createTezosApi } from "@ledgerhq/coin-tezos/api/index";
-import { TezosCoinConfig } from "@ledgerhq/coin-tezos/config";
+import type { TezosCoinConfig } from "@ledgerhq/coin-tezos/config";
 import type { AssetInfo } from "@ledgerhq/coin-module-framework/api/types";
 import type { CryptoCurrency, TokenCurrency } from "@ledgerhq/types-cryptoassets";
 import type { AccountReadiness } from "@ledgerhq/types-live";
